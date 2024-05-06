4d04541c5dad61d542ffab48f661574aa23a33ee
insuline <insuline.ru@gmail.com>
2024-05-06 15:36:38 +0400

    update handlers

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -32,50 +32,57 @@ PerpsV3MarketProxyContract_AccountCreated_handler(({ event, context }) => {
       created_at_block: BigInt(event.blockNumber),
       updated_at: BigInt(event.blockTimestamp),
       updated_at_block: BigInt(event.blockNumber),
+      feesPaid: BigInt(0),
+      liquidations: BigInt(0),
+      pnl: BigInt(0),
+      pnlWithFeesPaid: BigInt(0),
+      totalTrades: BigInt(0),
+      totalVolume: BigInt(0),
     })
   }
 })
 
 PerpsV3MarketProxyContract_PositionLiquidated_loader(({ event, context }) => {
-  context.PerpsV3Market.load(event.params.marketId.toString())
-  context.Account.load(event.params.accountId.toString())
+  const { marketId, accountId } = event.params
+  const positionId = `${marketId.toString()}-${accountId.toString()}`;
 
-  const positionId = `${event.params.marketId.toString()}-${event.params.accountId.toString()}`;
+  context.PerpsV3Market.load(marketId.toString())
   context.OpenPerpsV3Position.load(positionId, { loadPosition: { loadAccount: true } });
 })
 
 PerpsV3MarketProxyContract_PositionLiquidated_handler(({ event, context }) => {
-  const positionId = `${event.params.marketId.toString()}-${event.params.accountId.toString()}`;
+  const { marketId, accountId, amountLiquidated } = event.params
+  const marketIdStr = marketId.toString();
+  const accountIdStr = accountId.toString();
+
+  const positionId = `${marketIdStr}-${accountIdStr}`;
   const openPosition = context.OpenPerpsV3Position.get(positionId);
 
-  const account = context.Account.get(event.params.accountId.toString());
-  const market = context.PerpsV3Market.get(event.params.marketId.toString());
+  const account = context.Account.get(accountIdStr);
+  const market = context.PerpsV3Market.get(marketIdStr);
 
   if (!market) {
-    throw new Error(`Market ${event.params.marketId.toString()} not found`);
+    throw new Error(`Market ${marketIdStr} not found`);
   }
 
   if (!account) {
-    throw new Error(`Account ${event.params.accountId.toString()} not found`);
+    throw new Error(`Account ${accountIdStr} not found`);
   }
 
-  const estNotionalSize = getAbs(getAbs(event.params.amountLiquidated) * market.lastPrice / ETHER)
+  const estNotionalSize = getAbs(getAbs(amountLiquidated) * market.lastPrice / ETHER)
 
   const liquidationId = `${positionId}-${event.blockTimestamp.toString()}`;
   context.PositionLiquidation.set({
     id: liquidationId,
-    marketId: event.params.marketId,
-    accountId: event.params.accountId,
-    amount: event.params.amountLiquidated,
+    marketId: marketId,
+    accountId: accountId,
     accountOwner: account.owner,
+    amount: amountLiquidated,
     notionalAmount: estNotionalSize,
     estimatedPrice: market.lastPrice,
     timestamp: BigInt(event.blockTimestamp),
     position_id: positionId,
   })
-
-  const statId = `${event.params.accountId.toString()}-${account.owner}`;
-  const stat = context.PerpsV3Stat.get(statId);
 
   if (!openPosition) {
     console.warn(`Open position ${positionId} not found`);
@@ -95,70 +102,68 @@ PerpsV3MarketProxyContract_PositionLiquidated_handler(({ event, context }) => {
         position_id: undefined,
       })
 
-      if (stat) {
-        context.PerpsV3Stat.set({
-          ...stat,
-          liquidations: stat.liquidations + BigInt(1),
-        })
-      }
+      context.Account.set({
+        ...account,
+        liquidations: account.liquidations + BigInt(1),
+      })
     }
   }
 })
 
-PerpsV3MarketProxyContract_OrderSettled_loader(({ event, context }) => {
-  // const orderId = `${event.params.accountId.toString()}-${event.blockTimestamp.toString()}`;
-  const pendingOrderId = `${event.params.accountId.toString()}-${event.params.marketId.toString()}`;
-  const openPositionId = `${event.params.marketId.toString()}-${event.params.accountId.toString()}`;
+// PerpsV3MarketProxyContract_OrderSettled_loader(({ event, context }) => {
+//   // const orderId = `${event.params.accountId.toString()}-${event.blockTimestamp.toString()}`;
+//   const pendingOrderId = `${event.params.accountId.toString()}-${event.params.marketId.toString()}`;
+//   const openPositionId = `${event.params.marketId.toString()}-${event.params.accountId.toString()}`;
 
-  context.PendingOrder.load(pendingOrderId)
-  context.InterestCharged.load(`${event.params.accountId.toString()}-${event.transactionHash}`)
-  context.OpenPerpsV3Position.load(openPositionId, { loadPosition: { loadAccount: true, loadLiquidation: { loadPosition: undefined } } })
-  // context.PerpsV3Position.load
-  context.Account.load(event.params.accountId.toString())
-  // context.PerpsV3Stat.load
-  // context.PerpsV3Market.load()
+//   context.PendingOrder.load(pendingOrderId)
+//   context.InterestCharged.load(`${event.params.accountId.toString()}-${event.transactionHash}`)
+//   context.OpenPerpsV3Position.load(openPositionId, { loadPosition: { loadAccount: true, loadLiquidation: { loadPosition: undefined } } })
+//   // context.PerpsV3Position.load
+//   // context.Account.load(event.params.accountId.toString())
+//   // context.PerpsV3Stat.load
+//   // context.PerpsV3Market.load()
 
-})
+// })
 
-PerpsV3MarketProxyContract_OrderSettled_handler(({ event, context }) => {
-  const pendingOrderId = `${event.params.accountId.toString()}-${event.params.marketId.toString()}`;
-  const orderId = `${event.params.accountId.toString()}-${event.blockTimestamp.toString()}`;
+// PerpsV3MarketProxyContract_OrderSettled_handler(({ event, context }) => {
+//   const pendingOrderId = `${event.params.accountId.toString()}-${event.params.marketId.toString()}`;
+//   const orderId = `${event.params.accountId.toString()}-${event.blockTimestamp.toString()}`;
 
-  const pendingOrder = context.PendingOrder.get(pendingOrderId);
-  const account = context.Account.get(event.params.accountId.toString())
+//   const pendingOrder = context.PendingOrder.get(pendingOrderId);
+//   const account = context.Account.get(event.params.accountId.toString())
 
-  if (!account) {
-    console.warn(`Account ${event.params.accountId} not found`)
-    return
-  }
+//   if (!account) {
+//     console.warn(`Account ${event.params.accountId} not found`)
+//     return
+//   }
 
-  const openPositionId = `${event.params.marketId.toString()}-${event.params.accountId.toString()}`;
-  const openPosition = context.OpenPerpsV3Position.get(openPositionId)
+//   const openPositionId = `${event.params.marketId.toString()}-${event.params.accountId.toString()}`;
+//   const openPosition = context.OpenPerpsV3Position.get(openPositionId)
 
-  const interestCharged = context.InterestCharged.get(`${event.params.accountId.toString()}-${event.transactionHash}`)
+//   const interestCharged = context.InterestCharged.get(`${event.params.accountId.toString()}-${event.transactionHash}`)
 
-  const statId = `${account.id}-${account.owner}`
-  const stat = context.PerpsV3Stat.get()
-  context.OrderSettled.set({
-    id: orderId,
-    accountId: event.params.accountId,
-    account_id: event.params.accountId.toString(),
-    txnHash: event.transactionHash,
-    accruedFunding: event.params.accruedFunding,
-    collectedFees: event.params.collectedFees,
-    fillPrice: event.params.fillPrice,
-    marketId: event.params.marketId,
-    timestamp: BigInt(event.blockTimestamp),
-    totalFees: event.params.totalFees,
-    trackingCode: event.params.trackingCode,
-    settlementReward: event.params.settlementReward,
-    sizeDelta: event.params.sizeDelta,
-    newSize: event.params.newSize,
-    referralFees: event.params.referralFees,
-    settler: event.params.settler,
-    pnl: BigInt(0),
-    orderCommitted_id: pendingOrder?.id,
-    interestCharged: interestCharged?.interest,
-    position_id: openPosition?.position_id
-  })
-})
+//   const statId = `${account.id}-${account.owner}`
+//   const stat = context.PerpsV3Stat.get()
+//   context.OrderSettled.set({
+//     id: orderId,
+//     accountId: event.params.accountId,
+//     account_id: event.params.accountId.toString(),
+//     txnHash: event.transactionHash,
+//     accruedFunding: event.params.accruedFunding,
+//     collectedFees: event.params.collectedFees,
+//     fillPrice: event.params.fillPrice,
+//     marketId: event.params.marketId,
+//     timestamp: BigInt(event.blockTimestamp),
+//     totalFees: event.params.totalFees,
+//     trackingCode: event.params.trackingCode,
+//     settlementReward: event.params.settlementReward,
+//     sizeDelta: event.params.sizeDelta,
+//     newSize: event.params.newSize,
+//     referralFees: event.params.referralFees,
+//     settler: event.params.settler,
+//     pnl: BigInt(0),
+//     orderCommitted_id: pendingOrder?.id,
+//     interestCharged: interestCharged?.interest,
+//     position_id: openPosition?.position_id
+//   })
+// })
